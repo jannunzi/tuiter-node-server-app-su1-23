7c0cf59d6f011a53186b7acd0bfc780a1921bddb
Jose Annunziato <jose.annunziato@sonos.com>
2023-06-19 11:46:58 -0400

    secure true

--- app.js
+++ app.js
@@ -24,9 +24,12 @@ app.use(
   session({
     secret: "any string",
     resave: false,
-    saveUninitialized: true,
-    secure: true,
     proxy: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+    },
   })
 );
 
